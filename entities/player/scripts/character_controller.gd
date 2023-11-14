28e50973f7fb00edf5b6e4040f4100e152213caa
BedroomVampire <82517019+bedroomvampire@users.noreply.github.com>
2023-11-15 01:13:16 +0900

    Uploading the source project
    Title explains itself.

---
+++ entities/player/scripts/character_controller.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+
+const SPEED = 7.5
+const JUMP_VELOCITY = 11.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 1.5
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
