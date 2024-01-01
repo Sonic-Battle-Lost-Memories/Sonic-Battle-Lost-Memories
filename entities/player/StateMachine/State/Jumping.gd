ea86ae2ee6e289ab7999ac01f9e162ef29c2d4c1
Oreboat <111535650+Oreboat@users.noreply.github.com>
2024-01-01 11:56:54 -0600

    Update Jumping.gd

--- entities/player/StateMachine/State/Jumping.gd
+++ entities/player/StateMachine/State/Jumping.gd
@@ -1,66 +1,19 @@
-@icon("res://dev_material/nerd_moji.png")
-extends StateMachineState
-
-# time since this state started
-var time_elapsed:float = 0.0
+extends Node
 
 @export var jump_state_name = "Jumping"
-@export var lifetime:float = 0.3666
 
-#the name of the animation that this state will play
-@export var main_animation:String
-
-# identifies, in order, when each frame of the animation happens
-@export var frame_times:Array[float] = [
-	0.03333,
-	0.0833,
-	0.1333,
-	0.2,
-	0.2333,
-	0.28333
-	]
-var current_time_index = 0
-
-@onready var parent: Node = get_node("..")
-
-@export_group("transition")
-#state for touching the floor
-@export var on_grounded: StateMachineState
-
-#state for air-attack
-@export var on_primary: StateMachineState
-
-#state for when lifetime of the animation was exceeded.
-@export var on_timeout: StateMachineState
-
-@export var time_till_air_attack: float
-
-@export var initial_impulse: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-func setup(target:CharacterController):
-	target.sprite.animation = main_animation
-	target.sprite.frame = 0
-	time_elapsed = 0
-	
-	var inherited_direction = Vector2(target.velocity.x, target.velocity.z).normalized()
-	#initial impulse on character
-	target.velocity.x = (inherited_direction.x * initial_impulse.x)
-	target.velocity.z = (inherited_direction.y * initial_impulse.x)
-	#target.velocity.y = initial_impulse.y
-	
-	#target.allowing_double_jump = false
-	
-	current_time_index = 0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func step(target: CharacterController, delta):
 # Handle Jump.
-	#if Input.is_action_just_pressed("jump") and !target.respawning:
-		#target.jump()
-		#pass
-	
+	if Input.is_action_just_pressed("jump"):
+		target.jump()
+		pass
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
