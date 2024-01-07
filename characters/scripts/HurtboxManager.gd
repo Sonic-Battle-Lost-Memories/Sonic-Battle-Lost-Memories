944ef2777736ecd91b5a27a9c92d2b4bcb89531d
Oreboat <111535650+Oreboat@users.noreply.github.com>
2024-01-06 21:38:45 -0600

    fixed hurtbox bug

--- characters/scripts/HurtboxManager.gd
+++ characters/scripts/HurtboxManager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var Hurtboxes = $Hurtboxes
+@onready var Hurtboxes = $"Position Controller"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
